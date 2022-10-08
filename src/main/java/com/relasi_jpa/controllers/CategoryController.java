06a5499b93599b3c7b56b7bbbd861733557b7daa
seftianhd <48037525+seftianhd@users.noreply.github.com>
2022-10-08 13:56:19 +0700

    update relasi JPA

--- src/main/java/com/relasi_jpa/controllers/CategoryController.java
+++ src/main/java/com/relasi_jpa/controllers/CategoryController.java
@@ -2,81 +2,79 @@ package com.relasi_jpa.controllers;
 
 import javax.validation.Valid;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.relasi_jpa.dto.CategoryData;
 import com.relasi_jpa.dto.ResponseData;
-import com.relasi_jpa.models.entities.Product;
-import com.relasi_jpa.services.ProductService;
+import com.relasi_jpa.models.entities.Category;
+import com.relasi_jpa.services.CategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
-@RequestMapping("api/products")
-public class ProductController {
-    
+@RequestMapping("/api/catagories")
+public class CategoryController {
     @Autowired
-    private ProductService productService;
+    private CategoryService categoryService;
 
     @PostMapping
-    public ResponseEntity<ResponseData<Product>> create(@Valid @RequestBody Product product, Errors errors){
-        ResponseData<Product> responseData = new ResponseData<>();
-
+    public ResponseEntity<ResponseData<Category>> crate(@Valid @RequestBody CategoryData categoryData, Errors errors){
+        ResponseData<Category> responseData = new ResponseData<>();
         if(errors.hasErrors()){
-            for(ObjectError error : errors.getAllErrors()){
+            for (ObjectError error:errors.getAllErrors()){
                 responseData.getMessages().add(error.getDefaultMessage());
             }
-
             responseData.setStatus(false);
             responseData.setPayload(null);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
         }
-        responseData.setStatus(true);
-        responseData.setPayload(productService.save(product));
-        return ResponseEntity.ok(responseData);
-    }
 
-    @GetMapping
-    Iterable<Product> findAll(){
-        return productService.findAll();
+        Category category = new Category();
+        category.setName(categoryData.getName());
+
+        responseData.setStatus(true);
+        responseData.setPayload(categoryService.save(category));
+        return ResponseEntity.ok(responseData);
     }
 
     @GetMapping("/{id}")
-    public Product findById(@PathVariable("id") long id){
-        return productService.findOne(id);
+    public Category findOne(@RequestParam Long id) {
+        return categoryService.findOne(id);
+    }
+    
+    @GetMapping
+    public Iterable<Category> findAll(){
+        return categoryService.findAll();
     }
 
     @PutMapping
-    public ResponseEntity<ResponseData<Product>> update(@Valid @RequestBody Product product, Errors errors){
-        ResponseData<Product> responseData = new ResponseData<>();
-
+    public ResponseEntity<ResponseData<Category>> update(@Valid @RequestBody CategoryData categoryData, Errors errors){
+        ResponseData<Category> responseData = new ResponseData<>();
         if(errors.hasErrors()){
-            for(ObjectError error : errors.getAllErrors()){
+            for (ObjectError error:errors.getAllErrors()){
                 responseData.getMessages().add(error.getDefaultMessage());
             }
-
             responseData.setStatus(false);
             responseData.setPayload(null);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
         }
+
+        Category category = new Category();
+        category.setName(categoryData.getName());
+
         responseData.setStatus(true);
-        responseData.setPayload(productService.save(product));
+        responseData.setPayload(categoryService.save(category));
         return ResponseEntity.ok(responseData);
     }
-
-    @DeleteMapping("/{id}")
-    public void removeOne(@PathVariable("id") Long id){
-        productService.removeOne(id);
-    }
-
-    
 }
--- src/main/java/com/relasi_jpa/controllers/SupplierController.java
+++ src/main/java/com/relasi_jpa/controllers/SupplierController.java
@@ -1,10 +1,24 @@
 package com.relasi_jpa.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.relasi_jpa.dto.ResponseData;
+import com.relasi_jpa.dto.SupplierData;
+import com.relasi_jpa.models.entities.Supplier;
 import com.relasi_jpa.services.SupplierService;
+
 
 @RestController
 @RequestMapping("/api/suppliers")
@@ -12,5 +26,38 @@ public class SupplierController {
     @Autowired
     private SupplierService supplierService;
 
-    
+    @PostMapping
+    public ResponseEntity<ResponseData<Supplier>> create(@Valid @RequestBody SupplierData supplierData, Errors errors){
+        ResponseData<Supplier> responseData = new ResponseData<>();
+        if(errors.hasErrors()){
+            for(ObjectError error : errors.getAllErrors()){
+                responseData.getMessages().add(error.getDefaultMessage());
+            }
+            responseData.setStatus(false);
+            responseData.setPayload(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        
+        Supplier supplier = new Supplier();
+        supplier.setName(supplierData.getName());
+        supplier.setAddress(supplierData.getAddress());
+        supplier.setEmail(supplierData.getEmail());
+
+        responseData.setStatus(false);
+        responseData.setPayload(supplierService.save(supplier));
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/{id}")
+    public Supplier findOne(@PathVariable Long id){
+        return supplierService.findOne(id);
+    }
+
+    @GetMapping
+    public Iterable<Supplier> findAll(){
+        return supplierService.findAll();
+    }
+
+
+
 }
